6132ce7dbdb25df077032fb5cbb104fd81adf8eb
ROAR BIG CAT <34619082+Chillwave@users.noreply.github.com>
2023-01-04 23:30:37 -0500

    Update nanodrop.py

--- nanodrop.py
+++ nanodrop.py
@@ -1,17 +1,21 @@
-# Open the file in read-only mode
-with open('art.txt', 'r') as file:
-    # Read the contents of the file
-    readme = file.read()
-
-# Print the contents of the file to the console
-print(readme)
-
 import http.server
 import socketserver
 
 PORT = 8000
 
 Handler = http.server.SimpleHTTPRequestHandler
+
+# Open the file in read-only mode, with a default value of an empty string
+# in case the file does not exist
+try:
+    with open('art.txt', 'r') as file:
+        # Read the contents of the file
+        readme = file.read()
+except FileNotFoundError:
+    readme = ''
+
+# Print the contents of the file to the console
+print(readme)
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("serving at port", PORT)
